b3e1b91dd59f0c0eef9b7fe0d1d24fcbe3700b14
tom_glb <goodMorning_glb@atguigu..com>
2019-03-08 11:03:42 +0800

    增加代码注释

--- src/main/java/cn/finlab/blinddevice/service/WeatherService.java
+++ src/main/java/cn/finlab/blinddevice/service/WeatherService.java
@@ -10,26 +10,21 @@ public interface WeatherService {
 
     /**
      * 根据经纬度获取当前城市编码
-     * @param longitude 经度
-     * @param latitude 纬度
+     * @param longitude 小数点后最多六位
+     * @param latitude 小数点后最多六位
      * @return
+     * @throws GetWeatherException 获取城市编码异常
      */
     public String getAdcode(String longitude, String latitude) throws GetWeatherException;
 
     /**
      * 根据经纬度获取当前天气
-     * @param longitude 经度
-     * @param latitude 纬度
+     * @param longitude 经度 小数点后最多六位
+     * @param latitude 纬度 小数点后最多六位
      * @return
+     * @throws GetWeatherException 天气获取异常
      */
     public Weather getBaseWeather(String longitude, String latitude) throws GetWeatherException;
 
-    /**
-     * 根据经纬度获取天气预报
-     * @param longitude
-     * @param latitude
-     * @return
-     */
-    public Weather getAllWeather(String longitude, String latitude) throws GetWeatherException;
 
 }
